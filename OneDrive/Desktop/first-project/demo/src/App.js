89afa6b974fda638a1e81ee5e70934c5661ccf96
BuiQuangHa <hashimada2409@gmail.com>
2022-06-21 14:27:07 +0700

    first commit

---
+++ OneDrive/Desktop/first-project/demo/src/App.js
@@ -0,0 +1,78 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div class="App">
+      <div className="App1">
+        <p class="header">Đặt phòng ngay</p>
+
+        <div class="flexbox-container">
+          <div class="flexbox-item flexbox-item-1">
+            Địa điểm
+            <span class="material-icons">keyboard_arrow_down</span>
+          </div>
+          <div class="flexbox-item flexbox-item-2">
+            Ngày đến
+            <span class="material-icons">keyboard_arrow_down</span>
+          </div>
+          <div class="flexbox-item flexbox-item-3">
+            Ngày đi
+            <span class="material-icons">keyboard_arrow_down</span>
+          </div>
+          <div class="flexbox-item flexbox-item-4">Số người</div>
+          <div class="flexbox-item flexbox-item-5">Tìm kiếm</div>
+        </div>
+      </div>
+
+      <div class="App2">
+        <div class="Image-0">
+          PHỔ <br></br> BIẾN
+        </div>
+        <div class="Image-1">
+          HÀ <br></br> NỘI
+        </div>
+        <div class="Image-2">
+          ĐÀ <br></br> NẴNG
+        </div>
+        <div class="Image-3">
+          SÀI <br></br> GÒN
+        </div>
+        <div class="Image-4">
+          PHÚ <br></br> QUỐC
+        </div>
+      </div>
+
+      <div class="App3">
+        <h1> Loại hình </h1>
+        <div class="flex-image-container">
+          <div class="flex-image-1">
+            {" "}
+            Nhà <br></br>
+            biệt <br></br>
+            thự{" "}
+          </div>
+          <div class="block-234">
+            <div class="flex-image-2"> Phòng riêng</div>
+            <div class="block-34">
+              <div class="flex-image-3">
+                {" "}
+                Nhà <br></br>
+                chung <br></br>
+                cư{" "}
+              </div>
+              <div class="flex-image-4">
+                {" "}
+                Khu <br></br>
+                nghỉ<br></br>
+                dưỡng{" "}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+/*C:\Users\DELL\OneDrive\Desktop\first-project\demo\src\App.js  */
